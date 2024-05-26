62c7d4817150f3882e5c200f5c35f06dcae7f35b
MonBort <122515805+MonBort@users.noreply.github.com>
2024-05-26 13:29:33 +0800

    layout updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.googleGmsGoogleServices)
+    alias(libs.plugins.kotlinAndroid) // Reference Kotlin plugin from the version catalog
 }
 
 android {
@@ -39,26 +40,26 @@ android {
         enable = true
     }
 }
+
 dependencies {
     implementation("com.google.guava:guava:30.1.1-android")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
-    implementation("androidx.annotation:annotation:1.3.0")
+    implementation("androidx.annotation:annotation:1.8.0")
     implementation(libs.firebase.firestore)
     implementation(libs.firebase.storage)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-    implementation ("com.github.beigirad:ZigzagView:1.2.0")
-    implementation ("com.google.android.material:material:1.5.0")
+    implementation("com.github.beigirad:ZigzagView:1.2.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.cardview:cardview:1.0.0")
     implementation("de.hdodenhof:circleimageview:3.1.0")
-    implementation("com.github.bumptech.glide:glide:4.14.2")
-    annotationProcessor("com.github.bumptech.glide:compiler:4.14.2")
-    implementation("com.google.android.material:material:1.13.0-alpha01")
-    implementation("com.google.firebase:firebase-auth:21.0.0")
-    implementation ("androidx.work:work-runtime:2.9.0")
-
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
+    implementation("com.google.android.material:material:1.13.0-alpha02")
+    implementation("com.google.firebase:firebase-auth:23.0.0")
+    implementation("androidx.work:work-runtime:2.9.0")
 }
